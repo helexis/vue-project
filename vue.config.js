d1ccbb3425c6c311cf8cac88a0d7b32a479a6a57
helexis <helexis@gmail.com>
2023-08-01 23:21:34 +0900

    vue-router 기능 추가

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: config => {
+    /* prefetch 기능 삭제 */
+    config.plugins.delete('prefetch');
+  }
 })
